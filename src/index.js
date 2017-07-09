c6291ba3293f4574aa43e5b1efac1c2b27139f6b
Arjun Curat <nagarjun.curat@gmail.com>
2017-07-09 00:55:53 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = document.getElementById('container');
+
+const myDiv = <div>React Works!</div>;
+
+ReactDOM.render(myDiv, container);
